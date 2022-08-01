9c62ccea64ab3fd3341504c5ee412120d70e39c8
Smrutiranjan93 <103051369+Smrutiranjan93@users.noreply.github.com>
2022-08-01 23:17:23 +0530

    first commit

---
+++ switchcase.js
@@ -0,0 +1,12 @@
+var a=10;
+var b=20;
+var option='+';
+
+switch(option){
+    case '+':
+        console.log(a+b);
+        break;
+    case '-':
+            console.log(a-b);
+            break;    
+}
